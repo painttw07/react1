ad16205670d18d4f46b933909a9b2ddacbdf6b7c
painttw07 <painttw07@gmail.com>
2020-10-26 20:06:15 -0300

    Test

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './style.css';
 
 
 export default class Main extends Component {
-    render() {
+    state = { //variável estado do react
+        products: []
 
-        return <h1>Hello World!</h1>
+
+    };
+    componentDidMount() {
+        this.loadProducts();
+
+    }
+    loadProducts = async () => {
+        const response = await api.get('./products');
+
+        this.setState({ products: response.data.docs });
+
+
+    };
+    prevPage = () => { };
+    nextPage = () => { };
+
+
+    render() { //escutando a variavel de estado e atualiza conforme ela
+        const { products } = this.state;
+
+        return (
+            <div className="products-list">
+                {products.map(products => (
+                    <article key={products._id}>
+                        <strong>{products.title}</strong>
+                        <p>{products.description}</p>
+
+
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+                <div>
+                    <button onClick={this.prevPage}>Anterior</button>
+                    <button onClick={this.nextPage}>Próxima</button>
+                </div>
+            </div>
+        );
+        {/* <h1>Contagem produtos: {this.state.products.length}</h1> */ }
+
+
+        // return <h1>Hello</h1>
     }
 }
