87f34e23f832c59579dcc42b8feeddbfa237db55
painttw07 <painttw07@gmail.com>
2020-10-26 18:29:31 -0300

    commit 1

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+//variável que retorna algo
+const Header = () => <header id="main-header">JSHunt</header>;
+
+
+
+export default Header;
---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+
+export default class Main extends Component {
+    render() {
+
+        return <h1>Hello</h1>
+    }
+}
